4f1002fdf45af7ccd93700a37488108281020e38
EderlMarkus <38421306+EderlMarkus@users.noreply.github.com>
2019-05-21 11:00:33 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,19 +1,19 @@
 <?php
 
 //Username of CIS Technikum Wien 
-define('CIS_USERNAME', 'wi18b059');
+define('CIS_USERNAME', 'xxx');
 
 //Password of CIS Technikum Wien
-define('CIS_PASSWORD', 'Technikum32001');
+define('CIS_PASSWORD', 'xxx');
 
 //CSV-Link from Calender of CIS Technikum Wien
-define('CIS_KALENDERURL','https://cis.technikum-wien.at/cis/private/lvplan/stpl_kalender.php?type=student&pers_uid=wi18b059&ort_kurzbz=&stg_kz=256&sem=2&ver=B&grp=3&gruppe_kurzbz=&lva=&begin=1549062000&ende=1564610400&format=csv');
+define('CIS_KALENDERURL','xxx');
 
 //Define the Webhook of your Slack-Channel
-define('SLACK_CHANNEL','https://hooks.slack.com/services/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');
+define('SLACK_CHANNEL','xxx');
 
 //Define the Webhook of your Slack-Test-Channel
-define('SLACK_CHANNELTEST','https://hooks.slack.com/services/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');
+define('SLACK_CHANNELTEST','xxx');
 
 
 ?>
