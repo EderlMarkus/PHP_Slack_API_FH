1420142299d7838670ad0d0789cc7133bbc1cd1e
EderlMarkus <office@edma.at>
2019-05-21 10:55:46 +0200

    added dummy settings

---
+++ settings.php
@@ -0,0 +1,19 @@
+<?php
+
+//Username of CIS Technikum Wien 
+define('CIS_USERNAME', 'xxx');
+
+//Password of CIS Technikum Wien
+define('CIS_PASSWORD', 'xxx');
+
+//CSV-Link from Calender of CIS Technikum Wien
+define('CIS_KALENDERURL','xxx');
+
+//Define the Webhook of your Slack-Channel
+define('SLACK_CHANNEL','xxx');
+
+//Define the Webhook of your Slack-Test-Channel
+define('SLACK_CHANNELTEST','xxx');
+
+
+?>
